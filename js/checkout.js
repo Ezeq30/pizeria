64ac5082ea0657152313b6e94560abfe320fb0a5
ezeq30 <cdiazezequiel@hotmail.com>
2025-03-31 16:13:11 -0300

    se agrega el proyecto

--- js/checkout.js
+++ js/checkout.js
@@ -1,27 +1,73 @@
-
+let cardNuevo = []
 const tblody = document.querySelector("tbody")
 const btnComprar = document.querySelector("button#btnComprar")
+// const total = document.querySelector("total")
 
 const retornarTablaHTML =(producto) =>{ 
+    
    return ` <tr>
                 <th id="tablehead">
                 <img class="chekimg" src="${producto.imagen}"/>
                 </th>
                 <th id="tablehead">${producto.nombre}</th>
                 <th id="tablehead">${producto.precio}</th>
+                <th id="tablehead">${producto.cantidad}</th>
                 <th id="tablehead">
-                <button onclick="quitarDelCarrito(${producto.id})">Quitar</button>
+                <button onclick="quitarCardNuevo(${producto.id})">Quitar</button>
                 </th>
             </tr> ` 
 };
+///retorna los productos al carrito
 
 if(carritoFrutas.length > 0){
     tblody.innerHTML = ""
-    carritoFrutas.forEach((producto)=> (tblody.innerHTML += retornarTablaHTML(producto) )
-    )
+    const mapa = new Map() 
+    carritoFrutas.forEach((producto)=>{
+        if(!mapa.has(producto.id)){
+            mapa.set(producto.id,{...producto,cantidad:1})
+        }else{
+            const item = mapa.get(producto.id)
+            item.cantidad +=1
+            item.precio += producto.precio
+        }
+    })
+    
+    cardNuevo = Array.from (mapa.values())
+
+    cardNuevo.forEach((producto)=> {
+
+    return (tblody.innerHTML += retornarTablaHTML(producto) )
+    }
+)
+
+/// boton quitar
+}
+const quitarCardNuevo =(id)=>{
+
+
+    if(cardNuevo.length > 0 ){
+       for(let i = 0; i<cardNuevo.length; i++){
+        if(cardNuevo[i].id === id){
+            cardNuevo[i].cantidad -=1
+            
+        }
+       
+        console.log(cardNuevo.length)
+        console.log(cardNuevo[i])
+
+       }
+       retornarTablaHTML(cardNuevo)
+    }
 }
 
+
+
 btnComprar.addEventListener("click",()=>{
+    console.log('productos en el carrito',cardNuevo)
+    if(cardNuevo.length <= 0){
+        alert('Su carrito esta vacio')
+        return
+    }
     alert("Muchas Gracias Por Su Compra");
     localStorage.removeItem("carritoFrutas");
     tblody.innerHTML = ""
@@ -39,3 +85,6 @@ document.addEventListener("DOMContentLoaded",()=>{
     })
       
 })
+
+const total = document.querySelector("total")
+
